7f9d91ae976de210f97e3513f032135fdfa31b8a
Ranjitsinh Gavali <ranjitsinh425gavali@gmail.com>
2023-06-19 23:18:45 +0530

    task management
    task management assignment

---
+++ src/app/header/header.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+import { CartService } from 'src/app/service/cart.service';
+
+@Component({
+  selector: 'app-header',
+  templateUrl: './header.component.html',
+  styleUrls: ['./header.component.css']
+})
+export class HeaderComponent implements OnInit {
+  ngOnInit(): void {
+  }
+
+
+}
